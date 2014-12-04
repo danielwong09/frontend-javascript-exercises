0461edcfbf95cbece6e2868304075e07357a3006
dwong <daniel.wong09@gmail.com>
2014-12-04 23:23:40 +0000

    Completed slice and combine, findFirstMatch, findLastMatch, and find substringBetweenMatches functions

--- 04-strings/00-basics/challenge.js
+++ 04-strings/00-basics/challenge.js
@@ -11,12 +11,26 @@ module.exports.sliceItAndCombineIt = function(bigString, startA, endA, startB, e
 
 module.exports.findFirstMatch = function(text, searchString) {
 
+
+  return text.indexOf(searchString);
+
 };
 
 module.exports.findLastMatch = function(text, searchString) {
 
+  return text.lastIndexOf(searchString);
 };
 
 module.exports.substringBetweenMatches = function(text, searchString) {
+
+
+  var startIndex= text.indexOf(searchString);
+
+  startIndex= startIndex + searchString.length;
+
+  var endIndex= text.lastIndexOf(searchString);
+
+
+  return text.substring(startIndex, endIndex);
 
 };
