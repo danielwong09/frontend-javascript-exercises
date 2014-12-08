b1131987813d8e300fcb3224255f9c53f3d188b2
dwong <daniel.wong09@gmail.com>
2014-12-08 02:04:51 +0000

    Finished array definition fxns

--- 08-arrays/00_Array_Definition/challenge.js
+++ 08-arrays/00_Array_Definition/challenge.js
@@ -1,3 +1,9 @@
-module.exports.newArray = undefined;
+module.exports.newArray = function(item1,item2,item3,item4){
+  return [item1,item2,item3,item4];
+};
 
-module.exports.firstAndLast = undefined;
+module.exports.firstAndLast = function(initialArray){
+
+  return [initialArray[0], initialArray[initialArray.length-1]];
+
+};
