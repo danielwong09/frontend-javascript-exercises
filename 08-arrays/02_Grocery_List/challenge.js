7ca805f6a28eda133ffce80d47ce45a58a7d6a74
dwong <daniel.wong09@gmail.com>
2014-12-12 01:08:04 +0000

    Finished grocery list fxns

--- 08-arrays/02_Grocery_List/challenge.js
+++ 08-arrays/02_Grocery_List/challenge.js
@@ -1,3 +1,38 @@
-module.exports.addItem = undefined;
+module.exports.addItem = function(newItem,list){
 
-module.exports.reverseSortedList = undefined;
+/* This function should take two arguments:
+Don't add an item to the list if it already exists in the list*/
+
+
+
+//check if item is in the list
+
+var j = 0;
+
+  while (j < list.length){
+
+    if ( newItem != list[j]){
+      j++;
+    }else{
+      console.log("returning unaltered list");
+      return list;
+    };
+  };
+
+  console.log("should be pushing");
+  list.push(newItem);
+  return list;
+
+};
+
+
+
+module.exports.reverseSortedList = function(originalArray){
+
+  originalArray.sort();
+  originalArray.reverse();
+
+  return originalArray;
+
+};
+
