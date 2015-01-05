e088f2306479c22a712c0308932d6f2f96a68209
dwong <daniel.wong09@gmail.com>
2015-01-05 21:44:10 +0000

    created createCourse fxn

--- 09-objects/00-basics/challenge.js
+++ 09-objects/00-basics/challenge.js
@@ -1,4 +1,32 @@
-module.exports.createCourse = undefined;
+module.exports.createCourse = function(courseTitle, courseDuration, courseStudents){
+
+var course = {
+
+  title : courseTitle,
+  duration : courseDuration,
+  students : courseStudents
+
+
+};
+
+return course;
+
+};
+
+
+/*
+Complete the createCourse function. This function should:
+
+take three arguments that will define course properties
+courseTitle (string)
+courseDuration (string)
+courseStudents (array)
+return an object that has each property assigned its proper value
+
+*/
+
+
+
 
 module.exports.addProperty = undefined;
 
