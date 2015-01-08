f20f0bed2d86fdc2e330a33e40b6bedd7fd60ba9
dwong <daniel.wong09@gmail.com>
2015-01-08 23:23:12 +0000

    used fxns as condition and actions in while loop

--- 10-loops/03-while-loops/challenge.js
+++ 10-loops/03-while-loops/challenge.js
@@ -1,4 +1,19 @@
-module.exports.stream = undefined;
+module.exports.stream = function(conditionalFn, actionFn){
+
+  while (conditionalFn()){
+    actionFn();
+  }
+
+  return;
+
+};
+
+
+/*
+take two arguments, a conditionalFn and an actionFn.
+call actionFn until conditionalFn returns false.
+return nothing.
+*/
 
 module.exports.sumNumbers = undefined;
 
