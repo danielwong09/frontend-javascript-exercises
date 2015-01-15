d46f70fed9fcadbda2894262a4c41d74efc5fd7e
dwong <daniel.wong09@gmail.com>
2015-01-15 02:34:02 +0000

    used comparison in return statement and named object properties using strings to avoid if statements and be concise

--- 09-objects/00-basics/challenge.js
+++ 09-objects/00-basics/challenge.js
@@ -28,18 +28,86 @@ module.exports.addProperty = function(object, newProp, newValue){
   
 };
 
-/*Complete the formLetter function. This function should:
-
-take one argument, a letter, which has three properties recipient, sender, and msg
-combine the three properties into a single string with an additional greeting and closing
-insert additional new lines between the greeting, message, and signature.
-
-*/
-
 module.exports.formLetter = function(letter){
 
   return "Hello " + letter.recipient + ",\n\n" + letter.msg + "\n\nSincerely,\n" + letter.sender;
 
 };
 
-module.exports.canIGet = undefined;
+/*Complete the canIGet function. This function should:
+
+Take two arguments:
+item: represents what the user wants to buy
+money: represents how many dollars a user has
+return true if a user can afford a given item according to the price chart below, and false otherwise:
+'MacBook Air' - $999
+MacBook Pro' - $1299
+'Mac Pro' - $2499
+'Apple Sticker' - $1
+Return false if the item is not in the above list of Apple products
+Do this with 0 'if' conditions! (Hint: Place the above price table in an object).
+For example:
+
+canIGet('MacBook Air', 100) // returns false
+canIGet('MacBook Air', 1000) // returns true*/
+
+
+
+module.exports.canIGet = function(item,money){
+
+
+  var priceTable = {
+    "MacBook Air": 999,
+    "MacBook Pro": 1299,
+    "Mac Pro": 2499,
+    "Apple Sticker": 1
+  }
+
+  return priceTable[item] <= money;
+
+};
+
+
+/*
+
+
+  if (item =='MacBook Air') {
+    if (999<=money) {
+      return true;
+    } 
+    else{
+      return false;
+    };
+  }
+
+  if (item =='MacBook Pro') {
+    if (1299<=money) {
+      return true;
+    } 
+    else{
+      return false;
+    };
+  }
+
+  if (item =='Mac Pro') {
+    if (2499<=money) {
+      return true;
+    } 
+    else{
+      return false;
+    };
+  }
+
+  if (item =='Apple Sticker') {
+    if (1<=money) {
+      return true;
+    } 
+    else{
+      return false;
+    };
+  }
+
+  return false;
+
+
+*/
